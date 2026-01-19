95ebd7fafdad435de55bca139c2762546005bffa
bautistaprivitera <juanbautistaprivitera@gmail.com>
2026-01-19 20:42:26 -0300

    Paginate, filter & sort

--- controllers/products.controllers.js
+++ controllers/products.controllers.js
@@ -11,8 +11,35 @@ export const createProduct = async (req, res) => {
 
 export const getProduct = async (req, res) => {
     try {
-        const products = await Product.find();
-        res.json(products);
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 10;
+        const sort = req.query.sort;
+        const query = req.query.query;
+
+        const filter = query ? { title: { $regex: query, $options: 'i' } } : {};
+
+        const sortOptions = 
+        sort === 'asc' ? { price: 1 } :
+        sort === 'desc' ? { price: -1 } : {};
+
+        const result = await Product.paginate(filter, {
+        page, 
+        limit, 
+        sort: sortOptions,
+        lean:true
+        });
+
+        res.json({
+            status: 'success',
+            payload : result.docs,
+            totalPages : result.totalPages,
+            prevPage : result.prevPage,
+            nextPage : result.nextPage,
+            page : result.page,
+            hasPrevPage : result.hasPrevPage,
+            hasNextPage : result.hasNextPage
+        });
+
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
--- model/products.model.js
+++ model/products.model.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
+import paginate from 'mongoose-paginate-v2';
 
 const productSchema = new mongoose.Schema({
     title: { type: String, required: true },
     price: { type: Number, required: true },
     stock: { type: Number, required: true, default: 0 }
 })
+
+productSchema.plugin(paginate);
 
 export default mongoose.model('Product', productSchema);
